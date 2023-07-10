22948eb07e4a8257d4573ba5ebfc62ee44cc9d6d
ShanaySharif <Shanay.uw17@gmail.com>
2023-07-10 14:42:09 -0400

    add passing code for isosceles triangl

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -31,24 +31,23 @@ namespace ShapeTracker.Models
             _side3 = newValue;
         }
 
-        public string CheckType()
-        {
-            if ((Side1 > (Side2 + _side3)) || (Side2 > (Side1 + _side3)) || (_side3 > (Side1 + Side2)))
-            {
-                return "not a triangle";
-            }
-            else if ((Side1 != Side2) && ((Side1 != _side3)) && ((Side2 != _side3)))
-            {
-                return "scalene triangle";
-            }
-            else
-            {
-                return "";
-            }
-        }
+      public string CheckType()
+    {
+      if ((Side1 > (Side2 + _side3)) || (Side2 > (Side1 + _side3)) || (_side3 > (Side1 + Side2))) 
+      {
+        return "not a triangle";
+      } 
+      else if ((Side1 != Side2) && ((Side1 != _side3)) && ((Side2 != _side3))) 
+      {
+        return "scalene triangle";
+      }
+      else 
+      {
+        return "isosceles triangle";
+      }
     }
+  }
 }
-
 
 
 
