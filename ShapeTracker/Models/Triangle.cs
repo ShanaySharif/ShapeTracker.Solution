4fc061731f8e913d2608343e21806092786e2b6d
ShanaySharif <Shanay.uw17@gmail.com>
2023-07-10 09:53:23 -0400

    add Side1 get functionality + passing test

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -4,6 +4,27 @@ namespace ShapeTracker.Models
 {
   public class Triangle 
   {
+    private int _side1;
+    public int Side1
+    {
+
+        get { return _side1; }
+    }
+
+    public Triangle(int length1)
+    {
+
+        _side1 = length1;
+    }
+
+
+
+
+
+
+
+
+
 //     private int _side1;
 //     public int Side1
 //     {
